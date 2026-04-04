f0dbba463619b6c70138b26eecb3b5e118d014b0
van <van291630@gmail.com>
2026-04-04 15:38:42 +0300

    Quize analazing feature

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -43,45 +43,4 @@ export async function apiFetch(path, { method = "GET", body, auth = true } = {})
   }
   return data;
 }
-/*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  } catch {
-   *
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  } catch {'*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  } catch {'*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  // } catch {*
-ch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  // } catch {*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  // } catch {*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  // } catch {*
-  const res = await fetch(url, { method, headers, body: reqBody });
-  let data = null;
-  try {
-    data = await res.json();
-  } catch {*/
+
